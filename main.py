6a4085f13eeb39e2705813f87ca9a898f14209cc
Hannsikaa <175857804+Hannsikaa@users.noreply.github.com>
2026-03-15 02:54:35 +0530

    completed data validation

--- main.py
+++ main.py
@@ -8,24 +8,23 @@ app = FastAPI()
 def home():
     return {"message": "Habit Tracker API is running"}
 
-@app.get("/habits")
+@app.get("/habits",response_model=list[HabitResponse])
 def get_habits():
-    result=operations.get_habits()
-    return {"habits":result}
+    return operations.get_habits()
 
-@app.post("/habits")
+@app.post("/habits",response_model=MessageResponse)
 def add_habit(name: HabitCreate):
     result= operations.add_habits(name.name)
     if not result:
         raise HTTPException(status_code=400, detail="Habit already exists")
     return {"message": "Habit added"}
 
-@app.put("/habits/done")
+@app.put("/habits/done",response_model=MessageResponse)
 def mark_done(index: HabitIndex):
     operations.mark_habit_done([index.index])
     return {"message": "Habit marked done"}
 
-@app.put("/habits/change_name")
+@app.put("/habits/change_name",response_model=MessageResponse)
 def change_name(name: HabitRename):
     result=operations.change_habit_name(name.name,name.newname)
     if not result:
@@ -33,7 +32,7 @@ def change_name(name: HabitRename):
     return {"message": "Habit name changed"}
 
 #ikkada name direct url nundi teeskunnam so pydantic model avsaram led
-@app.delete("/habits/{name}")
+@app.delete("/habits/{name}",response_model=MessageResponse)
 def delete_habit(name: str):
     found = operations.delete_habit(name)
     if not found:
@@ -41,19 +40,17 @@ def delete_habit(name: str):
     return {"message": "Habit deleted"}
 
 # so ikkada name is a path parameter search?name=smtg ani vastadi in url so ikkada string ani teeskunnam instead of a pydantic model
-@app.get("/habits/search")
+@app.get("/habits/search",response_model=list[HabitResponse])
 def search_habit(name: str):
     result= operations.search_habit(name)
     if result=="Habit not found":
         raise HTTPException(status_code=404, detail="Habit not found")
-    return {"message": result}
+    return operations.search_habit(name)
 
-@app.get("/habits/leaderboard")
+@app.get("/habits/leaderboard",response_model=list[HabitResponse])
 def leaderboard_sort():
-    result= operations.leaderboard_sort()
-    return {"message": result}
+    return operations.leaderboard_sort()
 
-@app.get("/habits/stats")
+@app.get("/habits/stats",response_model=list[HabitResponse])
 def stats():
-    result= operations.stats_habits()
-    return {"message": result}
+    return operations.stats_habits()
--- schemas.py
+++ schemas.py
@@ -1,12 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 
 class HabitCreate(BaseModel):
-    name: str
+    name: str = Field(min_length=1, max_length=50)
+
+    @field_validator("name") # idi decorator ante name ni validate chese appudu kinda function run cheyyi ani
+    def remove_blank_names(cls, v): # cls ante paina cls
+        # v ante value ade name 
+        if not v.strip():# strip removes spaces at starting and ending of strings
+            raise ValueError("Habit name cannot be empty")
+        return v
 
 class HabitRename(BaseModel):
-    name: str
-    newname: str
+    name: str = Field(min_length=1, max_length=50)
+    newname: str = Field(min_length=1, max_length=50)
 
 class HabitIndex(BaseModel):
-    index: int
+    index: int = Field(gt=0) # gt ante greater than 0
 
+class HabitResponse(BaseModel):
+    id: int
+    name: str
+    status: bool
+    streak: int
+
+class MessageResponse(BaseModel):
+    message: str
